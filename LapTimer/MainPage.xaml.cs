827d48a2dad24f0902421cc8c62a968682eb9bc2
Scott Klein <scklein@microsoft.com>
2019-07-08 16:04:47 -0700

    Add project files.

---
+++ LapTimer/MainPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace LapTimer
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        int laps = 0;
+        int _Counter = 0;
+        DispatcherTimer timer = new DispatcherTimer();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void TxtLapCount_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            laps += 1;
+            //lbllaps.Content = laps;
+            txtLapCount.Text = laps.ToString();
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void BtnStop_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnReset_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnLapReset_Click(object sender, RoutedEventArgs e)
+        {
+            laps = 0;
+            txtLapCount.Text = laps.ToString();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer.Interval = TimeSpan.FromSeconds(1);
+            //timer.Tick += Timer_Tick;
+        }
+
+        public void Timer_Tick(object sender, RoutedEventArgs e)
+        {
+            _Counter += 1;
+            txtLapTime.Text = TimeSpan.FromSeconds(_Counter).ToString();
+        }
+
+    }
+}
